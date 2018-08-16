823f773480563d39f04883421695e2da7ed9398a
Ravi Shukla <ravi.shukla271288@gmail.com>
2018-08-16 17:02:41 -0500

    front page is added

---
+++ trainingshopping/src/main/java/com/ravi/trainingshopping/controller/PageController.java
@@ -0,0 +1,34 @@
+package com.ravi.trainingshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= {"/" , "/home" , "/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting" , "Welcome to Spring Trutorial");
+		return mv ;
+	}
+	
+	
+//	@RequestMapping(value= {"/test"})
+//	public ModelAndView test(@RequestParam(value ="greeting",required=false) String greeting) {
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting" , greeting);
+//		return mv ;
+//	}
+
+	
+	@RequestMapping(value= {"/test/{greeting}"})
+	public ModelAndView test(@PathVariable(value="greeting",required=false) String greeting) {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting" , greeting);
+		return mv ;
+	}
+}
